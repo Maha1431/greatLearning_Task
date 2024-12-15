1d155dc24188d7430fec396c43c31520cc3a123f
Maha <mmahaece07@gmail.com>
2024-12-15 12:57:26 +0530

    bugs resolved1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+import { useSelector } from 'react-redux';
 import Navbar from "./Navbar";
 const Board = lazy(() => import("./Board.jsx"));
 const Analytics = lazy(() => import("./Analytics.jsx"));
 const Settings = lazy(() => import("./Settings.jsx"));
-import { useSelector } from "react-redux";
-import "../css/Dashboard.css";
+import "../css/dashboard.css"
 import {
 	AddedPeople,
 	AddPeople,
@@ -15,21 +15,24 @@ import {
 } from "../components/Model";
 
 const Dashboard = () => {
-	const state = useSelector((store) => store.state);
-	return (
-		<>
-			<Navbar />
-			{state.dashboardSection == "board" && <Board />}
-			{state.dashboardSection == "analytics" && <Analytics />}
-			{state.dashboardSection == "settings" && <Settings />}
-			{state.addPeopleM && <AddPeople />}
-			{state.addedPeopleM && <AddedPeople />}
-			{state.logoutM && <Logout />}
-			{state.taskDeleteM && <TaskDelete />}
-			{state.taskCardM && <TaskCard />}
-			{state.updateCategoryM && <UpdateCategory />}
-		</>
-	);
+  const state = useSelector((store) => store.state);
+
+  return (
+    <>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        {state.dashboardSection === "board" && <Board />}
+        {state.dashboardSection === "analytics" && <Analytics />}
+        {state.dashboardSection === "settings" && <Settings />}
+      </Suspense>
+      {state.addPeopleM && <AddPeople />}
+      {state.addedPeopleM && <AddedPeople />}
+      {state.logoutM && <Logout />}
+      {state.taskDeleteM && <TaskDelete />}
+      {state.taskCardM && <TaskCard />}
+      {state.updateCategoryM && <UpdateCategory />}
+    </>
+  );
 };
 
 export default Dashboard;
